5e275bb68294896f8633b33484e6cb141633aa7d
Gaurav Rathod <gauravrathod472007@gmail.com>
2025-04-02 21:26:14 +0530

    Update tetris.cpp

--- tetris.cpp
+++ tetris.cpp
@@ -1,181 +1,225 @@
-#include <iostream>
-#include <vector>
-#include <windows.h> 
-#include <conio.h>   
-#include <ctime> 
+#include <bits/stdc++.h> 
+#include <conio.h>       
+#include <windows.h>     
 
 using namespace std;
 
-const int WIDTH = 10;
+int dropSpeed = 700;
+const int WIDTH = 15;
 const int HEIGHT = 20;
 
-// Tetromino shapes
-const vector<vector<vector<int>>> TETROMINOES = {
-    {{1, 1, 1, 1}}, // I
-    {{1, 1}, {1, 1}}, // O
-    {{0, 1, 0}, {1, 1, 1}}, // T
-    {{0, 1, 1}, {1, 1, 0}}, // S
-    {{1, 1, 0}, {0, 1, 1}}, // Z
-    {{1, 0, 0}, {1, 1, 1}}, // J
-    {{0, 0, 1}, {1, 1, 1}}  // L
-};
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+COORD cursorPos = {0, 0};
 
-class Tetromino {
-public:
-    vector<vector<int>> shape;
-    int x, y;
 
-    Tetromino(vector<vector<int>> s) {
-        shape = s;
-        x = WIDTH / 2 - s[0].size() / 2;
-        y = 0;
-    }
+struct Tetromino {
+    vector<vector<vector<int>>> rotations; 
+    string emoji; 
+    int rotationState; 
+
+    Tetromino(vector<vector<vector<int>>> r, string e) : rotations(r), emoji(e), rotationState(0) {}
 
     void rotate() {
-        int rows = shape.size(), cols = shape[0].size();
-        vector<vector<int>> rotated(cols, vector<int>(rows, 0));
+        rotationState = (rotationState + 1) % rotations.size(); 
+    }
 
-        for (int i = 0; i < rows; ++i)
-            for (int j = 0; j < cols; ++j)
-                rotated[j][rows - 1 - i] = shape[i][j];
-
-        shape = rotated;
+    vector<vector<int>> getShape() {
+        return rotations[rotationState];
     }
 };
 
-class Game {
-private:
-    vector<vector<int>> grid;
-    Tetromino* currentPiece;
-    int score;
-    bool gameOver;
+// Tetromino Definitions
+vector<Tetromino> shape = {
+    {{{{1}, {1}, {1}, {1}}, {{1, 1, 1, 1}}}, "🟦"}, 
+    {{{{1, 1}, {1, 1}}}, "🟨"},                     
+    {{{{1, 1, 1}, {0, 1, 0}}, 
+      {{0, 1}, {1, 1}, {0, 1}}, 
+      {{0, 1, 0}, {1, 1, 1}}, 
+      {{1, 0}, {1, 1}, {1, 0}}}, "🟪"},             
+    {{{{0, 1, 1}, {1, 1, 0}}, 
+      {{1, 0}, {1, 1}, {0, 1}}}, "🟩"},             
+    {{{{1, 1, 0}, {0, 1, 1}}, 
+      {{0, 1}, {1, 1}, {1, 0}}}, "🟥"},             
+    {{{{0, 1}, {0, 1}, {1, 1}}, 
+      {{1, 0, 0}, {1, 1, 1}}, 
+      {{1, 1}, {1, 0}, {1, 0}}, 
+      {{1, 1, 1}, {0, 0, 1}}}, "🟫"},               
+    {{{{1, 0}, {1, 0}, {1, 1}}, 
+      {{1, 1, 1}, {1, 0, 0}}, 
+      {{1, 1}, {0, 1}, {0, 1}}, 
+      {{0, 0, 1}, {1, 1, 1}}}, "🟧"}                // L piece
+};
 
-    bool checkCollision(const Tetromino& piece) {
-        for (int i = 0; i < piece.shape.size(); ++i) {
-            for (int j = 0; j < piece.shape[i].size(); ++j) {
-                if (piece.shape[i][j]) {
-                    int newX = piece.x + j;
-                    int newY = piece.y + i;
-                    if (newX < 0 || newX >= WIDTH || newY >= HEIGHT || (newY >= 0 && grid[newY][newX])) {
-                        return true;
-                    }
+vector<vector<string>> board(HEIGHT, vector<string>(WIDTH, "⬜"));
+Tetromino currentPiece = shape[0]; 
+Tetromino nextPiece = shape[0];    
+int pieceX = WIDTH / 2 - 1;
+int pieceY = 0;
+int score = 0;
+
+void moveCursor(int x, int y) {
+    cursorPos.X = x;
+    cursorPos.Y = y;
+    SetConsoleCursorPosition(hConsole, cursorPos);
+}
+
+void displayBoard() {
+    system("cls");
+    moveCursor(0, 0);
+    
+    ostringstream output;
+    
+    
+    output << "Next Piece:\n";
+    vector<vector<int>> nextShape = nextPiece.getShape();
+    for (int i = 0; i < nextShape.size(); i++) {
+        for (int j = 0; j < nextShape[i].size(); j++) {
+            if (nextShape[i][j] == 1)
+                output << nextPiece.emoji;
+            else
+                output << "  "; 
+        }
+        output << "\n";
+    }
+    output << "\n";
+    
+   
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (board[i][j] == "⬜" &&
+                pieceY <= i && i < pieceY + currentPiece.getShape().size() &&
+                pieceX <= j && j < pieceX + currentPiece.getShape()[0].size() &&
+                currentPiece.getShape()[i - pieceY][j - pieceX] == 1) {
+                output << currentPiece.emoji;
+            } else {
+                output << board[i][j];
+            }
+        }
+        output << "\n";
+    }
+    
+    output << "Score: " << score << "\n";
+    
+    cout << output.str();
+}
+
+bool isValidMove(Tetromino &piece, int x, int y) {
+    vector<vector<int>> shape = piece.getShape();
+    for (int i = 0; i < shape.size(); i++) {
+        for (int j = 0; j < shape[i].size(); j++) {
+            if (shape[i][j] == 1) {
+                int newX = x + j, newY = y + i;
+                if (newX < 0 || newX >= WIDTH || newY >= HEIGHT || board[newY][newX] != "⬜") {
+                    return false;
                 }
             }
         }
-        return false;
     }
+    return true;
+}
 
-    void mergePiece(const Tetromino& piece) {
-        for (int i = 0; i < piece.shape.size(); ++i) {
-            for (int j = 0; j < piece.shape[i].size(); ++j) {
-                if (piece.shape[i][j]) {
-                    grid[piece.y + i][piece.x + j] = 1;
+void placePiece() {
+    vector<vector<int>> shape = currentPiece.getShape();
+    for (int i = 0; i < shape.size(); i++) {
+        for (int j = 0; j < shape[i].size(); j++) {
+            if (shape[i][j] == 1) {
+                board[pieceY + i][pieceX + j] = currentPiece.emoji;
+            }
+        }
+    }
+}
+
+void clearLines() {
+    for (int i = HEIGHT - 1; i >= 0; i--) {
+        if (count(board[i].begin(), board[i].end(), "⬜") == 0) {
+            board.erase(board.begin() + i);
+            board.insert(board.begin(), vector<string>(WIDTH, "⬜"));
+            score += 100;
+            dropSpeed = max(100, dropSpeed - 50);
+        }
+    }
+}
+
+void spawnPiece() {
+   
+    currentPiece = nextPiece;
+    pieceX = WIDTH / 2 - 1;
+    pieceY = 0;
+    
+    
+    nextPiece = shape[rand() % shape.size()];
+    
+    if (!isValidMove(currentPiece, pieceX, pieceY)) {
+        int maxx = 0;
+        maxx = max(maxx, score);
+        cout << "Game Over! Final Score: " << score << "\n";
+        cout << "Maximum Score: " << maxx << "\n";
+        cout << "Press ENTER to play again or ESC to exit...";
+        Beep(1000, 800);
+        while (true) {
+            if (_kbhit()) {
+                char key = _getch();
+                if (key == 13) {
+                    score = 0;
+                    board.assign(HEIGHT, vector<string>(WIDTH, "⬜"));
+                    spawnPiece();
+                    return;
+                }
+                if (key == 27) {
+                    exit(0);
                 }
             }
         }
     }
+}
 
-    void clearLines() {
-        for (int i = HEIGHT - 1; i >= 0; --i) {
-            bool full = true;
-            for (int j = 0; j < WIDTH; ++j) {
-                if (!grid[i][j]) {
-                    full = false;
-                    break;
-                }
-            }
-            if (full) {
-                grid.erase(grid.begin() + i);
-                grid.insert(grid.begin(), vector<int>(WIDTH, 0));
-                score += 100;
-            }
+void handleInput() {
+    while (_kbhit()) {
+        char key = _getch();
+        int newX = pieceX, newY = pieceY;
+        Tetromino newPiece = currentPiece;
+
+        if (key == 75 && isValidMove(currentPiece, pieceX - 1, pieceY)) newX--; 
+        if (key == 77 && isValidMove(currentPiece, pieceX + 1, pieceY)) newX++; 
+        if (key == 80 && isValidMove(currentPiece, pieceX, pieceY + 1)) newY++; 
+        if (key == 72) { 
+            newPiece.rotate();
+            if (isValidMove(newPiece, pieceX, pieceY))
+                currentPiece = newPiece;
         }
-    }
-
-public:
-    Game() : grid(HEIGHT, vector<int>(WIDTH, 0)), score(0), gameOver(false) {
-        srand(static_cast<unsigned>(time(0)));
-        currentPiece = new Tetromino(TETROMINOES[rand() % TETROMINOES.size()]);
-    }
-
-    ~Game() {
-        delete currentPiece;
-    }
-
-    void handleInput() {
-        if (_kbhit()) {
-            char key = _getch();
-            Tetromino temp = *currentPiece;
-            switch (key) {
-                case 'a': temp.x--; break;
-                case 'd': temp.x++; break;
-                case 's': temp.y++; break;
-                case 'w': temp.rotate(); break;
-                case ' ':
-                    while (!checkCollision(temp)) temp.y++;
-                    temp.y--;
-                    break;
-                case 27: gameOver = true; break;
-            }
-            if (!checkCollision(temp)) *currentPiece = temp;
+        if (key == 32) 
+            while (isValidMove(currentPiece, pieceX, newY + 1)) newY++;
+        if (key == 27) { 
+            Beep(1000, 800);
+            exit(0);
         }
+        pieceX = newX;
+        pieceY = newY;
     }
+}
 
-    void update() {
-        Tetromino temp = *currentPiece;
-        temp.y++;
-        if (checkCollision(temp)) {
-            mergePiece(*currentPiece);
-            clearLines();
-            delete currentPiece;
-            currentPiece = new Tetromino(TETROMINOES[rand() % TETROMINOES.size()]);
-            if (checkCollision(*currentPiece)) gameOver = true;
+void gameLoop() {
+    while (true) {
+        handleInput();
+        if (isValidMove(currentPiece, pieceX, pieceY + 1)) {
+            pieceY++;
         } else {
-            *currentPiece = temp;
+            placePiece();
+            clearLines();
+            spawnPiece();
         }
+        displayBoard();
+        Sleep(dropSpeed);
     }
-
-    void render() {
-        system("cls");
-        vector<vector<int>> display = grid;
-
-        for (int i = 0; i < currentPiece->shape.size(); ++i) {
-            for (int j = 0; j < currentPiece->shape[i].size(); ++j) {
-                if (currentPiece->shape[i][j] && (currentPiece->y + i) >= 0) {
-                    display[currentPiece->y + i][currentPiece->x + j] = 1;
-                }
-            }
-        }
-
-        for (int i = 0; i < HEIGHT; ++i) {
-            for (int j = 0; j < WIDTH; ++j) {
-                cout << (display[i][j] ? "[]" : " .");
-            }
-            cout << endl;
-        }
-
-        cout << "Score: " << score << endl;
-        if (gameOver) cout << "Game Over!" << endl;
-    }
-
-    bool isGameOver() const {
-        return gameOver;
-    }
-};
+}
 
 int main() {
-    while (true) {
-        Game game;
-        while (!game.isGameOver()) {
-            game.handleInput();
-            game.update();
-            game.render();
-            Sleep(200);
-        }
-        cout << "Press 'i' to play again or any key to exit: ";
-        char choice;
-        cin >> choice;
-        if (choice != 'i') break;
-    }
+    srand(time(0));
+    SetConsoleOutputCP(CP_UTF8);
+
+    nextPiece = shape[rand() % shape.size()];
+    
+    spawnPiece();
+    gameLoop();
     return 0;
 }
